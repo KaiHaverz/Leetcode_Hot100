5efd5e6199fe8b35ff2856971321db948458d1f1
Lewis.Lee <837597708@qq.com>
2025-04-04 22:59:30 +0800

    update 2 pointers

--- 2 Two Pointers/15_threeSum.cpp
+++ 2 Two Pointers/15_threeSum.cpp
@@ -1,18 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
         vector<vector<int>> res;
-        if(nums.size() < 3) return res;
+        if (nums.size() < 3)
+            return res;
 
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size() - 2; i++){
-            
-        }
+        // sort(nums.begin(), nums.end());
+        // for(int i = 0; i < nums.size() - 2; i++){
+
+        // }
     }
 };
+
+int main()
+{
+    vector<int> nums = {-1, 0};
+
+    Solution s;
+    vector<vector<int>> res = s.threeSum(nums);
+    for (auto i : res)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Lewis.Lee's LeetCode Adventure 2025
+
+This repository contains my solutions to LeetCode problems.
+My goal is to accomplish the Hot100 and Top interview 150 in April and May 2025.
+
+
